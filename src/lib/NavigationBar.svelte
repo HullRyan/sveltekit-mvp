34f2eebec3d1949bfeaeb511333427c901cdee82
Ryan Hull <hull.ryanc@gmail.com>
2023-06-23 15:43:57 -0400

    Handle GitHub pages base url path

--- src/lib/NavigationBar.svelte
+++ src/lib/NavigationBar.svelte
@@ -1,25 +1,33 @@
 <script>
-    //This could be further improved by recursively checking the 
-    //path to hand any level of subfolders
+	//This could be further improved by recursively checking the
+	//path to hand any level of subfolders
 	import { page } from "$app/stores";
+	let basePath = $page.url.pathname.contains("/sveltekit-mvp")
+		? "/sveltekit-mvp"
+		: "/";
 	$: path = $page.url.pathname;
 </script>
 
 <nav class="flex gap-10 p-4 border-b-2">
-	<a href="/" class:font-bold={path == "/"}>Home</a>
-	<a href="/about" class:font-bold={path == "/about"}>About</a>
+	<a href={basePath} class:font-bold={path == basePath}>Home</a>
+	<a href={basePath + "/about"} class:font-bold={path == basePath + "/about"}
+		>About</a
+	>
 	<span
-		><a href="/classes" class:font-bold={path == "/classes"}>Classes</a>
-		{#if decodeURI(path.split("/")[2]) != "undefined"}
+		><a
+			href={basePath + "/classes"}
+			class:font-bold={path == basePath + "/classes"}>Classes</a
+		>
+		{#if decodeURI(path.split("/")[3]) != "undefined"}
 			/ <a
-				href="/{path.split('/')[1]}/{path.split('/')[2]}"
-				class:font-bold={path.split("/")[3] == undefined}
+				href="{basePath + '/' + path.split('/')[2]}/{path.split('/')[3]}"
+				class:font-bold={path.split("/")[4] == undefined}
 			>
-				{decodeURI(path.split("/")[2])}</a
+				{decodeURI(path.split("/")[3])}</a
 			>
 		{/if}
-		{#if decodeURI(path.split("/")[3]) != "undefined"}
-			/ <a href={path} class="font-bold"> {decodeURI(path.split("/")[3])}</a>
+		{#if decodeURI(path.split("/")[4]) != "undefined"}
+			/ <a href={path} class="font-bold"> {decodeURI(path.split("/")[4])}</a>
 		{/if}
 	</span>
 </nav>
