87bb0f699eaeeaafe7e2aa11a0135bbad4091b75
Ryan Hull <60150756+HullRyan@users.noreply.github.com>
2023-06-28 15:08:45 -0400

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,5 +4,5 @@
 
 <PageDisplay
 	title={"SvelteKit MVP Project - Ryan Hull\n ITCS 5102 - Programming Languages \nUniversity of North Caronlina at Charlotte"}
-	body={"The goal of this project is to create a website that displays markdown and converted Jupyter notebooks in a tree folder structure, using a web framework that I have no experience with. \nComparisons of Next.js/Nuxt3/SvelteKit were made, then ranked according to a set of requirements for this MVP, with SvelteKit coming our up top.\nJupyter's nbconvert was used to convert it's notebooks to html, and this web app uses slugs along with native html/mdsvex to display the html/markdown files. \n\nClick the Classes tab up top to learn more"}
+	body={"The goal of this project is to create a website that displays markdown and converted Jupyter notebooks in a tree folder structure, using a web framework that I have no experience with. \nComparisons of Next.js/Nuxt3/SvelteKit were made, then ranked according to a set of requirements for this MVP, with SvelteKit coming out on top.\nJupyter's nbconvert was used to convert it's notebooks to html, and this web app uses slugs along with native html/mdsvex to display the html/markdown files. \n\nClick the Classes tab up top to learn more"}
 />
